5da63826d96554f7eb18af8a258e86f3f12b0ad3
Alafresh <868@holbertonschool.com>
2020-04-14 14:21:20 -0500

    [Add] prints all possible combinations of two digits

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 9; i++)
+            {
+                for(int j = 1; j <= 9; j++)
+                {
+                    if (j > i)
+                        Console.Write("{0}{1}, ", i, j);
+                }
+            }
+        }
+    }
+}
