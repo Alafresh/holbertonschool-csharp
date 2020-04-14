633930892cee47788fb78478b2aa303b4ffd2513
Alafresh <868@holbertonschool.com>
2020-04-14 15:46:37 -0500

    programs that print a line

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10_print_line
+{
+    class Line
+    {
+        public static void PrintLine(int length)
+        {
+            string line = "_";
+            if (length <= 0)
+                Console.Write("\n");
+            for (int i = 1; i <= length; i++)
+            {
+                Console.Write(line);
+            }
+        }
+    }
+}
