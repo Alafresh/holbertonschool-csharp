0b9229c15d9a8fd79e2acb74d0fbba52e15da693
Alafresh <868@holbertonschool.com>
2020-04-14 14:01:29 -0500

    [Fix] add new line after the last number

--- 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -11,7 +11,7 @@ namespace _5_print_comb
             {
                 if(number == 99)
                 {
-                    Console.Write("{0}", number);
+                    Console.Write("{0}\n", number);
                     break;
                 }
                 Console.Write("{0:D2}, ", number);
