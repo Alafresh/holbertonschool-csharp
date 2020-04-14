17ac714b435a6bb8104df7981ae6242aa8e45233
Alafresh <868@holbertonschool.com>
2020-04-14 16:04:12 -0500

    Programs that prints  diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _11_print_diagonal
+{
+    class Line
+    {
+       public static void PrintDiagonal(int length)
+       {
+            string space = "";
+            string diagonal = "\\";
+            if (length <= 0)
+                Console.Write("\n");
+            for(int i = 1; i <= length; i++)
+            {
+                Console.WriteLine("{0}{1}", space, diagonal);
+                space += " ";
+            }
+       }
+    }
+}
