6c70057683a5c5bc84882cc1e9e4971a1f2268d1
Alafresh <868@holbertonschool.com>
2020-04-14 13:59:46 -0500

    program that prints number from 0 to 99

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while(number <= 99)
+            {
+                if(number == 99)
+                {
+                    Console.Write("{0}", number);
+                    break;
+                }
+                Console.Write("{0:D2}, ", number);
+                number++;
+            }
+        }
+    }
+}
