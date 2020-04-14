e4874e1ded7faae02413ec00c47fdbad963a972c
Alafresh <868@holbertonschool.com>
2020-04-13 20:17:01 -0500

    master [Update] changes the way to use the method ToString

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-        Console.WriteLine("Percent: " + percent.ToString("P"));
+        Console.WriteLine("Percent: " + percent.ToString(".00%"));
 		Console.WriteLine("Currency: " + currency.ToString("C2"));
 	}
 }
