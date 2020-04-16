f1261ac45ba7e0427aaadbe0aac35c67fe484e24
Alafresh <868@holbertonschool.com>
2020-04-16 01:45:24 -0500

    Retrieves an element from an array

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1_element_at
+{
+    class Array
+    {
+        public static int elementAt(int[] array, int index)
+        {
+            if (index < 0 || index >= array.Length)
+            {
+                Console.WriteLine("Index out of range");
+                return -1;
+            }
+            return array[index];
+        }
+    }
+}
