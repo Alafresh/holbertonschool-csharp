dcbb3c7e7516362674b51148935e2d7f1dd9692a
Alafresh <868@holbertonschool.com>
2020-04-16 18:24:50 -0500

    find the best score in dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int max = myList.Values.Max();
+        foreach (string key in myList.Keys)
+        {
+            if (myList[key] == max)
+                return key;
+        }
+        return myList.Keys.First();
+    }
+}
