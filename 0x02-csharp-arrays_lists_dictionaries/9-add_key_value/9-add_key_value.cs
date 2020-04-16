4693c4ac60f60d914d67e2be53f14fd23a6c3e8e
Alafresh <868@holbertonschool.com>
2020-04-16 15:15:06 -0500

    Update a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        bool flag = true;
+        foreach(KeyValuePair<string, string> entry in myDict)
+        {
+            if(key == entry.Key)
+            {
+                myDict[key] = value;
+                flag = false;
+                break;
+            }
+        }
+        if(flag)
+            myDict.Add(key, value);
+
+        return myDict;
+    }
+}
