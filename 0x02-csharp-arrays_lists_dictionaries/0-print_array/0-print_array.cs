2824781b17e3ad423e8575c92d0146d41eee7ed4
Alafresh <868@holbertonschool.com>
2020-04-16 13:06:36 -0500

    Add zero case

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -9,8 +9,14 @@
                 Console.WriteLine("Size cannot be negative");
                 return null;
             }
+            if (size == 0)
+            {
+                Console.Write("\n");
+                int[] myArray = new int[size];
+                return myArray;
+            }
             int[] numbers = new int[size];
-            for(int i = 0; i <= size; i++)
+            for(int i = 0; i < size; i++)
             {
                 numbers[i] = i;
                 if (i == size - 1)
