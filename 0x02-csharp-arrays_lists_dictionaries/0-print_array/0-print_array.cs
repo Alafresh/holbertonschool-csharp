ec9d3d7f162063a2a56b03d6d96c2bd3100552ca
Alafresh <868@holbertonschool.com>
2020-04-16 01:35:00 -0500

    program that creates and print an array

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _0_print_array
+{
+    class Array
+    {
+       public static int[] CreatePrint(int size)
+       {
+            if(size < 0)
+            {
+                Console.WriteLine("Size cannot be negative");
+                return null;
+            }
+            int[] numbers = new int[size];
+            for(int i = 0; i < size; i++)
+            {
+                numbers[i] = i;
+                if (i == size - 1)
+                {
+                    Console.WriteLine("{0}", numbers[i]);
+                    break;
+                }
+                Console.Write("{0} ", numbers[i]);
+            }
+            return numbers;
+       }
+    }
+}
