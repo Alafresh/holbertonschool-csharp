070e0f9e1d5b5a4147af7e31887bef97a02cfe66
Alafresh <868@holbertonschool.com>
2020-04-16 18:48:36 -0500

    rectangular square

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[5,5];
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if ((i == 2) && (j == 2))
+                        array[i,j] = 1;
+                    else
+                        array[i,j] = 0;
+                    if (j == 4)
+                        Console.WriteLine("{0}", array[i,j]);
+                    else
+                        Console.Write("{0} ", array[i, j]);
+                }
+            }
+        }
+    }
