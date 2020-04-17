67e9aace4ab94288a06e8c2426748c00c5df9d8c
Alafresh <868@holbertonschool.com>
2020-04-16 19:31:50 -0500

    creates and print a Jaggedd array

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[][] jaggedArray3 = 
+        {
+            new int[] {0, 1, 2, 3},
+            new int[] {0, 1, 2, 3, 4, 5, 6},
+            new int[] {0, 1}
+        };
+        for(int i = 0; i < jaggedArray3.Length; i++)
+        {
+            for(int j = 0; j < jaggedArray3[i].Length; j++)
+            {
+                if(j == jaggedArray3[i].Length - 1)
+                    Console.WriteLine("{0}", jaggedArray3[i][j]);
+                else
+                    Console.Write("{0} ", jaggedArray3[i][j]);
+            }
+        }
+    }
+}
+
