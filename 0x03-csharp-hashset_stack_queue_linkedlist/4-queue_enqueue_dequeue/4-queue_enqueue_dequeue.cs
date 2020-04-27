680496e2517c47aa61cf410a50b42f9136b965b4
Alafresh <868@holbertonschool.com>
2020-04-27 17:55:07 -0500

    FIFO

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+  public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+  {
+    Console.WriteLine("Number of items: {0}", aQueue.Count);
+    if (aQueue.Count == 0)
+      Console.WriteLine("Queue is empty");
+    else
+      Console.WriteLine("First item: {0}", aQueue.Peek());
+    if (aQueue.Contains(search))
+    {
+      foreach(string i in aQueue.ToArray())
+      {
+        aQueue.Dequeue();
+        if (i == search)
+          break;
+      }
+      Console.WriteLine("Queue contains \"{0}\": True", search);
+    }
+    else
+      Console.WriteLine("Queue contains \"{0}\": False", search);
+    aQueue.Enqueue(newItem);
+    return aQueue;
+  }
+}
