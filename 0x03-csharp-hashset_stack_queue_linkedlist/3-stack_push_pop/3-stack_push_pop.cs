f0abc1b65836bf48dfb099a56c28484564920e00
Alafresh <868@holbertonschool.com>
2020-04-27 17:13:03 -0500

    Stack structure in c#

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+  public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+  {
+    Console.WriteLine("Number of items: {0}", aStack.Count);
+    if (aStack.Count == 0)
+      Console.WriteLine("Stack is empty");
+    Console.WriteLine("Top item: {0}", aStack.Peek());
+    if (aStack.Contains(search))
+    {
+      foreach(string i in aStack.ToArray())
+      {
+        aStack.Pop();
+        if (i == search)
+          break;
+      }
+      Console.WriteLine("Stack contains {0}: True", search);
+    }
+    else
+      Console.WriteLine("Stack contains {0}: False", search);
+    aStack.Push(newItem);
+    return aStack;
+  }
+}
