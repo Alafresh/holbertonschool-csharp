69ff97c1b2872b199191eb828f77353931035091
Alafresh <868@holbertonschool.com>
2020-04-27 22:07:15 -0500

    Delete at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+  public static void Delete(LinkedList<int> myLList, int index)
+  {
+    int count = 0;
+    LinkedListNode<int> current = myLList.First;
+    while (current != null)
+    {
+      if (count == index)
+      {
+        myLList.Remove(current);
+        break;
+      }
+      count++;
+      current = current.Next;
+    }
+  }
+}
