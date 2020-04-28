1abe8580733165f9d01940489050fcf4e356f5ef
Alafresh <868@holbertonschool.com>
2020-04-27 20:06:15 -0500

    Add some conditions!

--- 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -5,7 +5,7 @@ class LList
 {
   public static int GetNode(LinkedList<int> myLList, int n)
   {
-    if (myLList.Count - 1 > n || myLList.Count == 0)
+    if (myLList.Count - 1 > n || myLList.Count <= 0)
       return 0;
     return myLList.ElementAt(n);
   }
