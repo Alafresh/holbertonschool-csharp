371f4e7d9b22c6b9030fd24c7d34ba042e0c960b
Alafresh <868@holbertonschool.com>
2020-04-28 15:22:20 -0500

    Safe list printing

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+  public static int SafePrint(List<int> myList, int n)
+  {
+    int i = 0;
+    try 
+    {
+      while(i < n)
+      {
+        Console.WriteLine(myList[i]);
+        i += 1;
+      }
+    }
+    catch 
+    {
+      
+    }
+    return i;
+  }
+}
