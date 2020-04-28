083b5442619acb277428e35474aa5179489bd526
Alafresh <868@holbertonschool.com>
2020-04-27 21:06:35 -0500

    Insert in order

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+  public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+  {
+    LinkedListNode<int> current = myLList.First;
+    LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+    while (current != null)
+    {
+      if (current.Value < n)
+        current = current.Next;
+      else
+      {
+        newNode = myLList.Find(current.Value);
+        myLList.AddBefore(newNode, n);
+        break;
+      }
+    }
+    return newNode;
+  }
+}
