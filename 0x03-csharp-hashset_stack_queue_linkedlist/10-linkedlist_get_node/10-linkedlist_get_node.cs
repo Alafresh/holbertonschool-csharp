332032eddaa1cbefc5b12b82244d573f3b7abb34
Alafresh <868@holbertonschool.com>
2020-04-27 19:54:23 -0500

    Get node at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+class LList
+{
+  public static int GetNode(LinkedList<int> myLList, int n)
+  {
+    return myLList.ElementAt(n);
+  }
+}
