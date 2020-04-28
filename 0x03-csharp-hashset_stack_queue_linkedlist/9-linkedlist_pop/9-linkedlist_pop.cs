66705a0ec0add37bf1fe7af63e74cac3ac06b9ff
Alafresh <868@holbertonschool.com>
2020-04-27 19:39:07 -0500

    Delete and return node

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+  public static int Pop(LinkedList<int> myLList)
+  {
+    int node;
+    if (myLList.Count == 0)
+      return 0;
+    node = myLList.First.Value;
+    return node;
+  }
+}
