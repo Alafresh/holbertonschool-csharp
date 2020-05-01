d6fe4653601949ee1b705e31f45aa1e1fac64352
Alafresh <868@holbertonschool.com>
2020-04-30 19:56:06 -0500

    Healthy competitiom

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Enemies
+{
+  
+/// <summary>Create an empty public class Zombie wit </summary>
+  public class Zombie
+  {
+    /// <summary>Represent the Health of enemie</summary>
+    public int health;
+    public Zombie()
+    {
+      health = 0;
+    }
+  }
+}
