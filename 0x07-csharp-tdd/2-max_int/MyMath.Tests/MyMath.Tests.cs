cf17e219059023df21f0fe0aa79fdcc5005e2f7e
Alafresh <868@holbertonschool.com>
2020-05-05 15:11:26 -0500

    Add more tests

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -9,7 +9,7 @@ namespace MyMath.Tests
         public void Test1()
         {
             // Arrange
-            List<int> myList = new List<int>() {1, 2, 3, 11};
+            List<int> myList = new List<int>() {1, 2, 11, 3, 4};
             // Act
             int result = MyMath.Operations.Max(myList);
             // Assert
@@ -24,6 +24,56 @@ namespace MyMath.Tests
             int result = MyMath.Operations.Max(myList);
             // Assert
             Assert.AreEqual(0, result);
+        }
+         [Test]
+        public void Test3()
+        {
+            // Arrange
+            List<int> myList = new List<int>() {11, 2, 1, 3, 4};
+            // Act
+            int result = MyMath.Operations.Max(myList);
+            // Assert
+            Assert.AreEqual(11, result);
+        }
+         [Test]
+        public void Test4()
+        {
+            // Arrange
+            List<int> myList = new List<int>() {1, 2, 4, 3, 11};
+            // Act
+            int result = MyMath.Operations.Max(myList);
+            // Assert
+            Assert.AreEqual(11, result);
+        }
+         [Test]
+        public void Test5()
+        {
+            // Arrange
+            List<int> myList = new List<int>() {-1, -2, -11, -3, -4};
+            // Act
+            int result = MyMath.Operations.Max(myList);
+            // Assert
+            Assert.AreEqual(-1, result);
+        }
+         [Test]
+        public void Test6()
+        {
+            // Arrange
+            List<int> myList = new List<int>() {2};
+            // Act
+            int result = MyMath.Operations.Max(myList);
+            // Assert
+            Assert.AreEqual(2, result);
+        }
+         [Test]
+        public void Test7()
+        {
+            // Arrange
+            List<int> myList = new List<int>() {1, 2, -11, 3, 4};
+            // Act
+            int result = MyMath.Operations.Max(myList);
+            // Assert
+            Assert.AreEqual(4, result);
         }
     }
 }
