881fcb2197e65feaca7cfb3020e4194d4fadbff1
Alafresh <868@holbertonschool.com>
2020-05-05 15:03:46 -0500

    changes the method to find the max num

--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -12,7 +12,13 @@ namespace MyMath
         {
             if (nums == null || nums.Count == 0)
                 return 0;
-            return nums.Max();
+            int result = nums[0];
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if (nums[i] > result)
+                    result = nums[i];
+            }
+            return result;
         }
     }
 }
