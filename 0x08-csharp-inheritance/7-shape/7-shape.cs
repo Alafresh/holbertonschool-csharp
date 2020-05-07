06cf3eb7bcc86d8ee719a38ce904157f8211edde
Alafresh <868@holbertonschool.com>
2020-05-07 16:27:26 -0500

    Full rectangle

---
+++ 0x08-csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+/// <summary>Base class Shape</summary>
+class Shape
+{
+    /// <summary>method useless</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>derived class from shape</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    /// <summary>Set and Get width property</summary>
+    public int Width
+    {
+        get => width;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                width = value;
+        }
+    }
+    /// <summary>Set and Get height property</summary>
+    public int Height
+    {
+        get => height;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                height = value;
+        }
+    }
+    /// <summary>Return the area of a Rectangle</summary>
+    public new int Area()
+    {
+        return height * width;
+    }
+    /// <summary>Return the measurement of a rectangle</summary>
+    public override string ToString()
+    {
+        return "[Rectangle] " + width + " / " + height;
+    }
+}
