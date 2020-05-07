9706fc75b91721c4f6b808373a27368302b6ab50
Alafresh <868@holbertonschool.com>
2020-05-07 14:53:05 -0500

    Only Subclass

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>Obj class</summary>
+class Obj
+{
+    /// <summary>check is a subclass of baseType</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if(derivedType.IsSubclassOf(baseType))
+            return true;
+        else
+            return false;
+    }
+}
