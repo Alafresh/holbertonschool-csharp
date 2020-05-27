ccce815e4435a5636b8a3a850b297be2c0e971fb
Alafresh <868@holbertonschool.com>
2020-05-26 23:04:28 -0500

    Vector scalar multiplication program

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>Class that has vectors functions</summary>
+class VectorMath
+{
+    /// <summary>multiplies a vector and a scalar and returns the resulting vector.</summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] errVector = {-1};
+        if (vector.Length < 2 || vector.Length > 3)
+            return errVector;
+
+        double[] result = new double[vector.Length];
+
+        for(int i = 0; i < vector.Length; i++)
+        {
+            result[i] = scalar * vector[i]; 
+        }
+        return result;
+    }
+}
