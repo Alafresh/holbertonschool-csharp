5c9329365476b93120db69fad94738898fa3fb56
Alafresh <868@holbertonschool.com>
2020-05-26 20:21:36 -0500

    Magnitude

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length < 2 || vector.Length > 3)
+            return -1;
+        double tmp = 0.0;
+        foreach(double v in vector)
+        {
+            tmp += v * v;
+        }
+        return (Math.Sqrt(tmp));
+    }
+}
