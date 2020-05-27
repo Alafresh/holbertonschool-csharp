ce5eef2ca9fd936b952782f75328c718e02edc79
Alafresh <868@holbertonschool.com>
2020-05-27 00:02:33 -0500

    Dot product of two vectors 2d or 3d

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary>Class that has vectors functions</summary>
+class VectorMath
+{
+    /// <summary>calculates dot product of either two 2D or two 3D vectors.</summary>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+         if ((vector1.Length < 2 || vector1.Length > 3) || (vector2.Length < 2 || vector2.Length > 3))
+            return -1;
+        else if (vector1.Length != vector2.Length)
+            return -1;
+        else
+        {
+            double[] product = new double[vector2.Length];
+            for(int i = 0; i < vector2.Length; i++)
+            {
+                product[i] = vector1[i] * vector2[i];
+            }
+            double dot = 0;
+            foreach(double v in product)
+            {
+                dot += v;
+            }
+            return dot;
+        }
+    }
+}
