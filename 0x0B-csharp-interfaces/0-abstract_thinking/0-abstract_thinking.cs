80b8d77f1490caf7bbe802f89bfab45923400315
Alafresh <868@holbertonschool.com>
2020-07-28 14:20:33 -0500

    Abstract thinking

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>Abstract thinking</summary>
+abstract class Base
+{
+    public string name;
+
+    /// <summary>Return the type</summary>
+    public override string ToString()
+    {
+        return name + " is a " + GetType();
+    }
+}
