f827f84fa278b7769cf545555f116a44b7ab7bac
Alafresh <868@holbertonschool.com>
2020-07-28 17:06:12 -0500

    Escape Room

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+/// <summary>Abstract thinking</summary>
+abstract class Base
+{
+    /// <summary>Property</summary>
+    public string name = "";
+
+    /// <summary>Return the type</summary>
+    public override string ToString()
+    {
+        return name + " is a " + GetType();
+    }
+}
+
+/// <summary>Interface Interactive</summary>
+interface IInteractive
+{
+    void Interact();
+}
+/// <summary>Interface Breakable</summary>
+interface IBreakable
+{
+    int durability { get; set;}
+    void Break();
+}
+/// <summary>Interface Collectable</summary>
+interface ICollectable
+{
+    bool isCollected {get; set;}
+    void Collect();
+}
+/// <summary>New class Door</summary>
+class Door : Base, IInteractive
+{
+    public Door(string value)
+    {
+        if (name == "")
+            name = "Door";
+        else
+            name = value;
+    }
+    public void Interact()
+    {
+        Console.WriteLine("You try to open the " + name + ". It's locked.");
+    }
+}
