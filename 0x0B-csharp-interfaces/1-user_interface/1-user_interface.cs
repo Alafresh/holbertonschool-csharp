28a2395076c72260ce7b1f6b6c696a38ef2ad4d6
Alafresh <868@holbertonschool.com>
2020-07-28 15:45:02 -0500

    User interface

---
+++ 0x0B-csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+/// <summary>Abstract thinking</summary>
+abstract class Base
+{
+    /// <summary>Property</summary>
+    public string name = "";
+
+    /// <summary>Return the type</summary>
+    public override string ToString()
+    {
+        return name + " is a " + GetType();
+    }
+}
+
+interface IInteractive
+{
+    void Interact()
+    {
+
+    }
+}
+interface IBreakable
+{
+    int durability { get; set;}
+    void Break()
+    {
+
+    }
+}
+
+interface ICollectable
+{
+    bool isCollected {set; get;}
+    void Collect()
+    {
+
+    }
+}
+
+class TestObject : IInteractive , IBreakable, ICollectable
+{
+    public int durability {get; set;}
+    public bool isCollected {set; get;}
+}
