fa2c55745a5c8899c76fb5e3d8e28caeafac7b17
Alafresh <868@holbertonschool.com>
2020-07-29 14:25:13 -0500

    Universal Health

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>Abstract thinking</summary>
+public class Player
+{
+    private string name;
+
+    private float maxHp;
+    private float hp;
+
+    /// <summary>Constructor</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            this.maxHp = 0f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+    /// <summary>HP status</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health.");
+    }
+}
