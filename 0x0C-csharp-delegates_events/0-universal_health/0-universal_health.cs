38900d12c468ebbc9623c179803eceda21b8436f
Alafresh <868@holbertonschool.com>
2020-07-29 15:01:52 -0500

    Universal health

--- 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -13,7 +13,7 @@ public class Player
     /// <summary>Constructor</summary>
     public Player(string name = "Player", float maxHp = 100f)
     {
-        if (maxHp <= 0)
+        if (maxHp < 0)
         {
             this.maxHp = 100f;
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
