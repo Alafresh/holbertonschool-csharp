9f937c178dae235b5dfa7acd6c37f2813606afeb
Alafresh <868@holbertonschool.com>
2020-07-29 15:50:57 -0500

    Damage delegation

---
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+delegate float CalculateHealth();
+
+/// <summary>Abstract thinking</summary>
+public class Player
+{
+    /// <summary>Constructor</summary>
+    protected string name;
+    /// <summary>Constructor</summary>
+    protected float maxHp;
+    /// <summary>Constructor</summary>
+    protected float hp;
+
+    /// <summary>Constructor</summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp < 0)
+        {
+            this.maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        else
+            this.maxHp = maxHp;
+        this.name = name;
+        this.hp = this.maxHp;
+    }
+    /// <summary>HP status</summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine(this.name + " has " + this.hp + " / " + this.maxHp + " health");
+    }
+    /// <summary>Damage status</summary>
+    public void TakeDamage(float damage)
+    {
+        if (damage < 0)
+            Console.WriteLine(this.name + " takes 0 damage!");
+        else    
+            Console.WriteLine(this.name + " takes " + damage + " damage!");
+    }
+
+    /// <summary>Heal status</summary>
+    public void HealDamage(float heal)
+    {
+        if (heal < 0)
+            Console.WriteLine(this.name + " heals 0 damage!");
+        else
+            Console.WriteLine(this.name + " heals " + heal + " HP!");
+    }
+}
